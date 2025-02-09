286e2607d434d13eda4d8fe15868cb68ba35c068
galuh wikri <galuhwikri05@gmail.com>
2025-02-09 12:27:24 +0700

    Menyelesaikan Tugas1

---
+++ Tugas1/src/IndeksNilaiMatkul.kt
@@ -0,0 +1,31 @@
+fun main() {
+    var nomor = 1
+    while (true) {
+        print("Nilai ke-$nomor: ")
+        val input = readLine()?.trim()
+
+        // Menangani input kosong atau nilai yang tidak valid
+        val nilai = input?.toIntOrNull()
+        if (nilai == null) {
+            println("Nilai harus diisi dan berupa angka.")
+        } else {
+            println("$nomor $input ${getIndeks(nilai)}")
+        }
+        nomor++
+    }
+}
+
+// Fungsi untuk menentukan indeks berdasarkan nilai
+fun getIndeks(nilai: Int?): String {
+    return when {
+        nilai == null -> "Nilai harus diisi"
+        nilai in 80..100 -> "A"
+        nilai in 70..79 -> "AB"
+        nilai in 60..69 -> "B"
+        nilai in 50..59 -> "BC"
+        nilai in 40..49 -> "C"
+        nilai in 30..39 -> "D"
+        nilai in 0..29 -> "E"
+        else -> "Nilai di luar jangkauan"
+    }
+}
---
+++ Tugas1/src/PersegiPanjang.kt
@@ -0,0 +1,27 @@
+import java.util.Scanner
+
+fun main() {
+
+    val scanner = Scanner(System.`in`)
+    print("Masukkan panjang: ")
+    val panjang = scanner.nextDouble()
+    print("Masukkan lebar: ")
+    val lebar = scanner.nextDouble()
+
+    val persegiPanjang = PersegiPanjang(panjang, lebar)
+    println("\nHasil Perhitungan Persegi Panjang:")
+    println("Panjang: $panjang")
+    println("Lebar: $lebar")
+    println("Luas: ${persegiPanjang.hitungLuas()}")
+    println("Keliling: ${persegiPanjang.hitungKeliling()}")
+}
+
+class PersegiPanjang(private val panjang: Double, private val lebar: Double) {
+    fun hitungLuas(): Double {
+        return panjang * lebar
+    }
+
+    fun hitungKeliling(): Double {
+        return 2 * (panjang + lebar)
+    }
+}
